7f3ea5a6a98847ec3d585967c9598672286bc36f
gianluigi97 <gianluigi.mosti97@gmail.com>
2025-03-19 21:48:42 +0100

    scegliere l'utente e inserire il quantitativo (19/03/2025)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# WYB
-PWA 
+tabella users (id, nome, email)
+tabella record (nome, litri, data, ricordo)
+
--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 from flask import Flask, render_template
-import mysql.connector
+import sqlite3
+import pandas as pd 
 
 app = Flask(__name__)
 
+class Database:
+
+    dbname = "utenti_db"
+
+    @classmethod
+    def connection(cls):
+        return sqlite3.connect(cls.dbname)
+
+    
+
 @app.route("/")
-def homepage():
+def index():
 
-    conn = mysql.connector.connect(
-            host = 'localhost', 
-            user = 'root',
-            password = 'gianrootluigi',
-            database = 'webapp'
-        )
-
+    conn = Database.connection()
     cur = conn.cursor()
-    nomi = cur.execute("""SELECT name FROM users""")
-        
-    nomi = nomi.fetchall()
-
+    results = cur.execute("""SELECT nome FROM users""")
+    nomi = [row[0] for row in cur.fetchall()]
 
     return render_template("homepage.html", nomi=nomi)
+    
 
 if __name__ == "__main__":
 
     app.run(debug=True)
+
+    # conn = Database.connection()
+    # cur = conn.cursor()
+    # cur.execute("""SELECT * FROM users""")
+    # print(cur.fetchall())

c6e88535e8576f958f519ff411ebcaae326b1bd3
gianluigi97 <gianluigi.mosti97@gmail.com>
2025-03-28 18:49:13 +0100

    changes (28/03/2025)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
-import pandas as pd 
 
 app = Flask(__name__)
 
@@ -12,24 +11,53 @@ class Database:
     def connection(cls):
         return sqlite3.connect(cls.dbname)
 
+    @classmethod
+    def addRecord(cls, nome, litri):
+
+        conn = cls.connection()
+        cur = conn.cursor()
+
+        cur.execute("""INSERT INTO record(nome, litri) VALUES (?, ?)""", (nome, litri))
+        conn.commit()
+
+        return 
     
+
 
 @app.route("/")
 def index():
-
     conn = Database.connection()
     cur = conn.cursor()
+
     results = cur.execute("""SELECT nome FROM users""")
     nomi = [row[0] for row in cur.fetchall()]
 
-    return render_template("homepage.html", nomi=nomi)
+    start = 2025
+    actual = cur.execute("""Select sum(litri) from record""").fetchone()[0]
+    totale = start - actual
+
+    return render_template("homepage.html", nomi=nomi, totale=totale)
+   
     
+@app.route("/")
+def record():
+    conn = Database.connection()
+    
+    if request.method == "POST":
+        nome = request.form['nome']
+        litri = request.form['litri']
+
+        Database.addRecord(nome, litri)
+
+    return render_template("homepage.html")
+
+
+
+
+
 
 if __name__ == "__main__":
 
     app.run(debug=True)
 
-    # conn = Database.connection()
-    # cur = conn.cursor()
-    # cur.execute("""SELECT * FROM users""")
-    # print(cur.fetchall())
+    

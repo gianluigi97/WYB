dceb6adc77bbc3345d235c7df3562ffd4363a635
gianluigi97 <gianluigi.mosti97@gmail.com>
2025-04-09 09:40:40 +0200

    reset main.py to render server

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def serve_sw():
     #app.run(debug=True)
 
 if __name__ == "__main__":
-    #port = int(os.getenv("PORT", 5000))
-    #app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
 
